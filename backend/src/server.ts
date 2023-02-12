a02d277ac5029d92501c3d95a56dc9aa1e973286
Fernanda Costa <fer-mcosta@hotmail.com>
2023-02-11 22:06:53 -0300

    Retorno de linhas para teste

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import axios from 'axios'
 
 const PORT = process.env.PORT || 4000
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
@@ -9,7 +10,26 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(cors({ origin: ['http://localhost:3000']}))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+})
+
+app.get('/', (req, res) => {
+
+    axios({
+        method: "get",
+        url: "https://transporteservico.urbs.curitiba.pr.gov.br/getPontosLinha.php?linha=870&c=98ad8",
+      })
+      .then(function (response) {
+        res.send( response.data)
+      });
+
+})
+
+app.use(cors({ origin: '*'}))
 
 app.use((req, res) => res.status(404));
 
